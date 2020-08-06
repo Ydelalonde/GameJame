5d66e77a4680f5794a2c0174152d527186dd19de
ydelalonde <ydelalonde@gmail.com>
2020-08-06 17:45:44 +0200

    Add Sound Add bruitage princess

---
+++ GameJamBrackeys2020.2/Assets/Script/NoReplaySound.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NoReplaySound : MonoBehaviour
+{
+
+    private int actualSceneIndex;
+    static Object instance = null;
+
+    void Awake()
+    {
+        if (instance != null )
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    public void Update()
+    {
+        actualSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (actualSceneIndex == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (instance == this)
+        {
+            instance = null;
+        }
+    }
+}
